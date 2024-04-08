96e91baf43f2d8173929e77ba5e5fc908417382e
sanitole <159641685+sanitole@users.noreply.github.com>
2024-04-08 02:50:27 -0400

    Update user.tf

--- Class2/user.tf
+++ Class2/user.tf
@@ -15,7 +15,7 @@ resource "aws_iam_group" "hello" {
 #   name = "tf-testing-group-membership"
 
 #   users = [
-#     for i in aws_iam_user.lb : i.name  # for i in resource(aws_iam_user).resourece name(lb)
+#     for i in aws_iam_user.lb : i.name  # for i in resource(aws_iam_user).resource name(lb)
 #   ]
 
 #   group = aws_iam_group.hello.name
