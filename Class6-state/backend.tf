2765cc1a8d66ea2636f67e6ebde81c9aac8591d8
sanitole <nauseanow@gmail.com>
2024-04-13 17:34:56 +0000

    update

---
+++ Class6-state/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "kaizen-test-bucket"
+    key    = "ohio/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
--- Class6-state/main.tf
+++ Class6-state/main.tf
@@ -5,4 +5,9 @@ provider "aws" {
 resource "aws_key_pair" "deployer" {
   key_name = "kaizen"
   public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Team = "DevOps"
+    Env = "Dev"
+  }
 }
